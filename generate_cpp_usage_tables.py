4cd829994d247744f044bc8ecabf131766e6f509
Jean-Noel Goumain <contact@jn-goumain.ca>
2023-08-06 11:37:42 -0400

    moved the genrated hid_usages files to another folder (nested folders content is not used when building a project)

--- generate_cpp_usage_tables.py
+++ generate_cpp_usage_tables.py
@@ -2,6 +2,11 @@
 # It's embedded in the HID Usage Tables PDF file, as an attachment
 # and can be extracted with a tool like https://www.konwerter.net/en/ExtractPdfAttachments/ 
 
+# the generated files are written in the generated_hid_usages folder
+
+# only the "consumer.h", "generic_desktop.h" and "keyboard_keypad.h" files are used in the project
+# thet are manully copied in the "ble_client_hid" component folder
+
 import json
 import re
 with open("HidUsageTables.json", "r", encoding='utf-8') as hid_usages_file:
@@ -10,7 +15,7 @@ with open("HidUsageTables.json", "r", encoding='utf-8') as hid_usages_file:
     for page in usage_pages:
         page_name : str = page["Name"]
         page_name = re.sub('[^a-zA-Z0-9]+','_',page_name)
-        with open(f"./components/ble_client_hid/hid_usages/generated/{page_name.lower()}.h", "w", encoding='utf-8') as header_file:
+        with open(f"./generated_hid_usages/{page_name.lower()}.h", "w", encoding='utf-8') as header_file:
             usage_page : str = f'#define USAGE_PAGE_{page_name.upper()} {{\"{page["Name"]}\", {{\\\n'
             for usage in page["UsageIds"]:
                 safe_usage_name = usage["Name"].replace(chr(0x2010), '-') # fix ambiguous '-' char
